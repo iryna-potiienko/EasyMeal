93e2d0e731a4b932260d74cd03384236ebc4fd87
iryna-potiienko <iryna.potiienko@gmail.com>
2017-08-17 13:20:15 +0300

    Такая кодировка подходит?

--- Form.py
+++ Form.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
+# -*- coding: cp1251 -*-
 import write_recipes
 from os import listdir
 from os.path import isfile
 from os.path import join as joinpath
-#import ImageTkInter   # Р”РѕР±Р°РІРёС‚СЊ РіРёРїРµСЂР»С‹РЅРє (РєРЅРѕРїРєР° Р·Р°РєР°Р·Р°С‚СЊ С‚РѕРІР°СЂС‹)
-                                # Р’СЃС‚Р°РІРёС‚СЊ Р»РѕРіРѕС‚РёРї
-from tkinter import *       # РџСЂРѕР±Р»РµРјС‹ СЃ СЂР°СЃС€РёСЂРµРЅРёРµРј
+#import ImageTkInter   # Добавить гиперлынк (кнопка заказать товары)
+                                # Вставить логотип
+from tkinter import *       # Проблемы с расширением
 
 root = Tk()
 root.title('EasyMeal')
@@ -13,13 +13,13 @@ root.geometry('500x300')
 root['bg'] = '#3dbde0'
 #image = Image.open("lenna.jpg")
 #photo = ImageTk.PhotoImage(image)
-#im = Image.open('С„РѕРЅ.jpg')
+#im = Image.open('фон.jpg')
 #ph_im = ImageTk.PhotoImage(im)
 #bd = Label(root, image=bg_image)
 #bd.pack()
 
 def sorting(event):
-    '''Р¤СѓРЅРєС†РёСЏ РєРѕС‚РѕСЂР°СЏ РѕС‚РєСЂС‹РІР°РµС‚ РґРѕС‡РµСЂРЅРµРµ РѕРєРЅРѕ СЃ СЂРµС†РµРїС‚РѕРј Рё РІС‹Р±РѕСЂРєРѕР№ РёРЅРіСЂРµРґРёРµРЅС‚РѕРІ'''
+    '''Функция которая открывает дочернее окно с рецептом и выборкой ингредиентов'''
     def exit(event):
         root.quit()
     def buy_products(event):
@@ -42,39 +42,39 @@ def sorting(event):
     frame_1.pack(side = LEFT)
     frame_2 = Frame(root, height=500, width=1500, bg = '#3dbde0')
     frame_2.place(x = 0, y = 750)
-    label = Label(frame_1, text='РљР»РёРєРЅРёС‚Рµ РЅР° РёРЅРіСЂРµРґРёРµРЅС‚С‹ РєРѕС‚РѕСЂРёС… Сѓ РІР°СЃ РЅРµС‚Сѓ:', font='Calibri 14 bold italic')
+    label = Label(frame_1, text='Кликните на ингредиенты которих у вас нету:', font='Calibri 14 bold italic')
     # label.place(x = 50, y = 20)
     label.pack()
 
     file = open(filename + c + '.txt')
+    #file = file.unicode(file, 'utf-8').encode('cp1251')
+    #file.close()
     #a = file.readline(1.0)
     #file.delete(1.0, 2.0)
     #for i, list in enumerate(file):
-     #   if i >= 2:              # С‡С‚РµРЅРёРµ С„Р°Р№Р»Р° РЅР°С‡РёРЅР°СЏ СЃРѕ РІС‚РѕСЂРѕР№ СЃС‚СЂРѕС‡РєРё
+     #   if i >= 2:              # чтение файла начиная со второй строчки
     a = file.readlines(12)
     b = file.readlines(1)
     list = file.readline()
     list = list.rstrip('\n')
 
-    while list != '':   # С†РёРєР» РґР»СЏ РѕРїСЂРµРґРµР»РµРЅРёСЏ РєР°РєРёРµ РїСЂРѕРґСѓРєС‚С‹ РµСЃС‚СЊ РґР»СЏ РїСЂРёРіРѕС‚РѕРІР»РµРЅРёСЏ, Р° РєР°РєРёС… РЅРµС‚Сѓ
+    while list != '':   # цикл для определения какие продукты есть для приготовления, а каких нету
                     if list == a or list == b:
                         lab = Label(root)
-                    if list[0:2:1] == 'Р”Р»СЏ':
+                    if list[0:2:1] == 'Для':
                         lab1 = Label()
                     check = Button(frame_1, text=list, width = 60, bg = '#3dbde0', font = 'Calibri 13 italic')
                     check.pack(side = TOP)
                     list = file.readline()
                     list = list.rstrip('\n')
 
-
-                    def change(event):   # РїСЂРё РЅР°Р¶Р°С‚РёРё РЅР° РєРЅРѕРїРєСѓ РѕРЅР° СЃС‚Р°РЅРµС‚ РєСЂР°СЃРЅРѕР№ Рё РїСЂРѕРґСѓРєС‚ РїРѕСЏРІРёС‚СЃСЏ РІ С‚РµРєСЃС‚РѕРІРѕРј РїРѕР»Рµ РІРЅРёР·Сѓ
+                    def change(event):   # при нажатии на кнопку она станет красной и продукт появится в текстовом поле внизу
                         global a
                         event.widget['bg'] = 'red'
-                        print(event.widget['text'])
                         text.insert(END, event.widget['text'])
                         text.insert(END, '\n')
 
-                    def returning(event):   # РїСЂРё РґРІРѕР№РЅРѕРј РЅР°Р¶Р°С‚РёРё РЅР° РєРЅРѕРїРєСѓ С„СѓРЅРєС†РёСЏ РѕС‚РјРµРЅРёС‚ С„СѓРЅРєС†РёСЋ change()
+                    def returning(event):   # при двойном нажатии на кнопку функция отменит функцию change()
                         event.widget['bg'] = '#3dbde0'
                         text.replace(event.widget['text'], "")
                         #text.delete()
@@ -87,20 +87,20 @@ def sorting(event):
     label_title.place(x = 600, y = 5)
     text_recipe = Text(root, wrap = WORD, width = 60, height = 25)
     text_recipe.insert(1.0, file)
-    button_ok = Button(root, text='Р’С‹Р№С‚Рё', bg = '#3dbde0', font = 'Calibri 14 bold')
+    button_ok = Button(root, text='Выйти', bg = '#3dbde0', font = 'Calibri 14 bold')
     button_ok.bind('<Button-1>', exit)
     button_ok.place(x = 1325, y = 5)
-    lab = Label(root, text = 'Рђ РІРѕС‚ С‡С‚Рѕ РЅР°РґРѕ РґРѕРєСѓРїРёС‚СЊ:', font = 'Calibri 14 bold italic')
+    lab = Label(root, text = 'А вот что надо докупить:', font = 'Calibri 14 bold italic')
     lab.place(x = 800, y = 475)
     text = Text(root, width = 55, height = 13)
     text.place(x = 800, y = 520)
     text_recipe.place(x = 800, y = 60)
-    button_search = Button(root, text = 'Р—Р°РєР°Р·Р°С‚СЊ РЅРµРґРѕСЃС‚Р°СЋС‰РёРµ РїСЂРѕРґСѓРєС‚С‹', bg = '#3dbde0', font = 'Calibri 14 bold')
+    button_search = Button(root, text = 'Заказать недостающие продукты', bg = '#3dbde0', font = 'Calibri 14 bold')
     button_search.bind('<Button-1>', buy_products)
     button_search.place(x = 1025, y= 5)
 
 def file_title(event):
-    '''Р¤СѓРЅРєС†РёСЏ РєРѕС‚РѕСЂР°СЏ РїРѕРєР°Р·С‹РІР°РµС‚ СЃРїРёСЃРѕРє С‚РµРєСЃС‚РѕРІС‹С… С„Р°Р№Р»РѕРІ РІ РїР°РїРєРµ - СЃРїРёСЃРѕРє СЂРµС†РµРїС‚РѕРІ РІС‹Р±СЂР°РЅРЅРѕР№ РєР°С‚РµРіРѕСЂРёРё'''
+    '''Функция которая показывает список текстовых файлов в папке - список рецептов выбранной категории'''
     global filename
     root = Toplevel()
     root.title('EasyMeal')
@@ -110,9 +110,9 @@ def file_title(event):
     frame2 = Frame(root, width = 500, height = 300)
     frame3 = Frame(root, width = 500, height = 100, bg = '#3dbde0')
     frame3.place(x = 0, y = 500)
-    label = Label(frame2, text = 'Р’С‹Р±РµСЂРёС‚Рµ Р±Р»СЋРґРѕ:', font = 'Calibri 20 bold italic')
+    label = Label(frame2, text = 'Выберите блюдо:', font = 'Calibri 20 bold italic')
     label.pack()
-    mypath = 'Р РµС†РµРїС‚С‹/' + event.widget['text']
+    mypath = 'Рецепты/' + event.widget['text']
     for i in listdir(mypath):
         if isfile(joinpath(mypath,i)):
             i = i[0:-4]
@@ -125,7 +125,7 @@ def file_title(event):
     frame2.pack()
 
 def new_window(event):
-    '''Р¤СѓРЅРєС†РёСЏ РєРѕС‚РѕСЂР°СЏ РѕС‚РєСЂС‹РІР°РµС‚ РѕРєРЅРѕ СЃ СЂРµС†РµРїС‚Р°РјРё'''
+    '''Функция которая открывает окно с рецептами'''
     def closing(event):
         window.destroy()
     window = Toplevel(root)
@@ -134,14 +134,14 @@ def new_window(event):
     window['bg'] = '#3dbde0'
     frame = Frame(window, width = 620, height = 350)
     frame.pack()
-    lab1 = Label(window, font = 'Calibri 20 bold italic', text=''' РҐРѕСЂРѕС€Рѕ, РјС‹ Р±СѓРґРµРј РіРѕС‚РѕРІРёС‚СЊ! 
-    Р§С‚Рѕ РјС‹ С…РѕС‚РёРј РїСЂРёРіРѕС‚РѕРІРёС‚СЊ СЃРµРіРѕРґРЅСЏ?''')
-    but3 = Button(window, text='РЎР°Р»Р°С‚С‹', width=15, height = 2, bg = '#3dbde0', font = 'Calibri 15 bold italic')
-    but4 = Button(window, text='РџРµСЂРІРѕРµ Р±Р»СЋРґРѕ', width=15, height = 2, bg = '#3dbde0', font = 'Calibri 15 bold italic')
-    but5 = Button(window, text='РћСЃРЅРѕРІРЅРѕРµ Р±Р»СЋРґРѕ', width=15, height = 2, bg = '#3dbde0', font = 'Calibri 15 bold italic')
-    but6 = Button(window, text='Р“Р°СЂРЅРёСЂС‹', width=15, height = 2, bg = '#3dbde0', font = 'Calibri 15 bold italic')
-    but7 = Button(window, text='Р”РµСЃРµСЂС‚С‹', width=15, height = 2, bg = '#3dbde0', font = 'Calibri 15 bold italic')
-    but8 = Button(window, text='РќР°РїРёС‚РєРё', width=15, height = 2, bg = '#3dbde0', font = 'Calibri 15 bold italic')
+    lab1 = Label(window, font = 'Calibri 20 bold italic', text=''' Хорошо, мы будем готовить! 
+    Что мы хотим приготовить сегодня?''')
+    but3 = Button(window, text='Салаты', width=15, height = 2, bg = '#3dbde0', font = 'Calibri 15 bold italic')
+    but4 = Button(window, text='Первое блюдо', width=15, height = 2, bg = '#3dbde0', font = 'Calibri 15 bold italic')
+    but5 = Button(window, text='Основное блюдо', width=15, height = 2, bg = '#3dbde0', font = 'Calibri 15 bold italic')
+    but6 = Button(window, text='Гарниры', width=15, height = 2, bg = '#3dbde0', font = 'Calibri 15 bold italic')
+    but7 = Button(window, text='Десерты', width=15, height = 2, bg = '#3dbde0', font = 'Calibri 15 bold italic')
+    but8 = Button(window, text='Напитки', width=15, height = 2, bg = '#3dbde0', font = 'Calibri 15 bold italic')
 
     but3.bind('<Button-1>', file_title)
     but4.bind('<Button-1>', file_title)
@@ -162,9 +162,9 @@ def new_window(event):
 frame1 = Frame(root, width = 500, height = 230)
 frame1.pack()
 
-lab = Label(root,text='РџСЂРёРІРµС‚, С‡С‚Рѕ РґРµР»Р°РµРј СЃРµРіРѕРґРЅСЏ?', font = 'Calibri 20 bold italic')
-but1 = Button(root,text='РіРѕС‚РѕРІРёРј',width=15, bg = '#3dbde0', font = 'Calibri 15 bold italic')
-but2 = Button(root,text='СЃРѕР·РґР°РµРј',width=15, bg = '#3dbde0', font = 'Calibri 15 bold italic')
+lab = Label(root,text='Привет, что делаем сегодня?', font = 'Calibri 20 bold italic')
+but1 = Button(root,text='готовим',width=15, bg = '#3dbde0', font = 'Calibri 15 bold italic')
+but2 = Button(root,text='создаем',width=15, bg = '#3dbde0', font = 'Calibri 15 bold italic')
 
 but1.bind('<Button->',new_window)
 but2.bind('<Button-1>', write_recipes.create_recipe)
